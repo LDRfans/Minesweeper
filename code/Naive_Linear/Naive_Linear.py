dc596a0990433d57121814ad1c21f098ed0c22c1
YOUNG1018 <young1018@126.com>
2020-07-03 02:40:29 +0800

    make the player

--- code/Naive_Linear/Naive_Linear.py
+++ code/Naive_Linear/Naive_Linear.py
@@ -7,29 +7,46 @@ class Naive_Linear:
         self.dim_2 = dim_2
         self.input = input
         # weights: w_0 to w_8
-        self.weights = np.random.rand(8+1)
+        # self.weights = np.ones(2+1)
+        self.weights = np.random.rand(2+1)
         # output
         self.output = 0
         # average probability to be a mine
         self.average_prob = mine_num / (dim_1*dim_2)
 
     def padding(self,input):
-        padding_array = np.zeros([self.dim_1+2, self.dim_2+2])
-        # for i in range(self.dim_1):
-        #     for j in range(self.dim_2):
+        # padding around the field
+        padding_array = np.ones([self.dim_1+2, self.dim_2+2])*self.average_prob*3
         padding_array[1:self.dim_1+1,1:self.dim_2+1] = input[0:self.dim_1,0:self.dim_2]
         return padding_array
 
-    def sweep(self):
+    def travel(self):
+        # output: probs
+        probs = np.zeros([8,8])
         # padding input by 1
         self.input = self.padding(self.input)
         # 3x3 zone to handle
         zone = np.zeros((3,3))
-        # 
+        # travel through the field, with 3x3 zone
         for i in range(8):
             for j in range(8):
                 zone[0:3,0:3] = self.input[i:i+3, j:j+3]
-                zone[np.isnan(zone)] = self.average_prob
-            w = np.sum(zone)
+                zone[np.isnan(zone)] = self.average_prob * 8
+                # probs[i,j] = np.sum(zone)
+                probs[i,j] = self.calculate_prob(zone)
+        return probs
 
-        return w
+    def calculate_prob(self, zone):
+        # local weights
+        weights = self.weights
+        weights_sum = weights[0]+weights[1]
+        weights[0] = weights[0] / weights_sum
+        weights[1] = weights[1] / weights_sum
+
+        # zone: 2 dimension -> x: 1 dimension
+        x = np.zeros(9)
+        for i in range(3):  x[i*3:i*3+3] = zone[i,...]
+        
+        # calculate prob
+        prob = weights[0]*(x[0]+x[2]+x[6]+x[8]) + weights[1]*(x[1]+x[3]+x[5]+x[7]) + weights[2]*1
+        return prob
--- code/Naive_Linear/Naive_Play.py
+++ code/Naive_Linear/Naive_Play.py
@@ -1,13 +1,35 @@
+import numpy as np
+
 from Naive_Linear import Naive_Linear
 from MineSweeper import MineSweeper
 
-game = MineSweeper()
-naive_player = Naive_Linear(game.state, 8, 8, 10)
+def naive_play():
+    turn = 0
+    game = MineSweeper()
+    # init: reveal the corner
+    game.selectCell([0,0])
+    # play the game
+    while(not game.gameOver):
+        turn += 1
+        print(game.state)
+        naive_player = Naive_Linear(game.state, 8, 8, 10)
+        min = [0, 0, naive_player.average_prob*8*9]
+        # find the min_prob tile
+        prob_matrix = naive_player.travel()
+        print(prob_matrix)
+        if game.victory == True:    break
+        for i in range(8):
+            for j in range(8):
+                if np.isnan(game.state[i,j]):  # must be nan
+                    if prob_matrix[i,j] <= min[2]:
+                        min[0] = i
+                        min[1] = j
+                        min[2] = prob_matrix[i,j]
+        if min[0] == 0 and min[1] == 0:
+            # choose a random tile?
+            print("Random...?")
+        print(min)
+        game.selectCell([min[0],min[1]])
+    return turn
 
-# pick a tile
-game.selectCell([0,0])
-
-print(naive_player.sweep())
-print(naive_player.input)
-print(naive_player.average_prob)
-# print(naive_player.output)
+# print(game.mines)
